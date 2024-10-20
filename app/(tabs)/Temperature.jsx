77c4e7ad648e197f3478b7ae94b258130da16ae0
K13R15 <kier.cunanan30@gmail.com>
2024-10-20 19:21:42 +0800

    updated iot code

--- app/(tabs)/Temperature.jsx
+++ app/(tabs)/Temperature.jsx
@@ -1,13 +1,16 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import HomeScreen from "./home";
 
-const TemperatureScreen = ({ navigation }) => {
+const TemperatureScreen = ({}) => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text>Temperature Details</Text>
       <Button
         title="Go back to Dashboard"
-        onPress={() => navigation.goBack()}
+        onPress={() => navigation.goBack("home")}
       />
     </View>
   );
